5e5b509b713fbd064d6bde91790d663abd3a7ec1
PhanHiep2025 <phanhiepcc2000@gmail.com>
2025-12-01 13:10:40 +0700

    Final

--- Src/Website_Ban_Giay/ShoesStore/obj/Debug/net7.0/ShoesStore.AssemblyInfo.cs
+++ Src/Website_Ban_Giay/ShoesStore/obj/Debug/net7.0/ShoesStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoesStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cff3058101a5323b07e4c3031075e92169c7964")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoesStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoesStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
