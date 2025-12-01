299410e439e13f86e85f29cec758b51b1e4ae546
PhanHiep2025 <phanhiepcc2000@gmail.com>
2025-12-01 11:23:44 +0700

    bcapajj nhật đồ án

--- Src/Website_Ban_Giay/ShoesStore/obj/Debug/net7.0/ShoesStore.AssemblyInfo.cs
+++ Src/Website_Ban_Giay/ShoesStore/obj/Debug/net7.0/ShoesStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoesStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa71471ff62643e6edb573bb78aae54a522af43b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoesStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoesStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
