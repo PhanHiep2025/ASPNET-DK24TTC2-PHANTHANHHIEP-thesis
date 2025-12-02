9da53d06d8715877214101d35050be326a9392f3
PhanHiep2025 <phanhiepcc2000@gmail.com>
2025-12-02 10:18:48 +0700

    comit

---
+++ Src/Website_Ban_Giay/ShoesStore/ViewModels/AddressNoteBookViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ShoesStore.Models;
+
+namespace ShoesStore.ViewModels
+{
+    public class AddressNoteBookViewModel
+    {
+        public string HoTen { get; set; }
+        public string Sdt { get; set; }
+        public List<Tinh> tinhList { get; set; }
+        public List<Phuong> phuongList { get; set; }
+    }  
+}
